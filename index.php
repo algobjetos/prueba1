6cdbfb4353540f588a17119cbe28c78d237e8004
algobjetos <algobjetos@gmail.com>
2017-12-08 15:26:47 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html;charset=UTF-8">  
+        <title>Prueba</title>
+    </head>
+    
+    <body>
+    
+    
+    <?php
+    
+    print "<h1> Prueba de Despliegue  </h1>   \n";
+    
+  
+    
+    print "<p>    Aqui hay cosas   </p>   \n";
+    
+  
+    
+    ?>
+    </body>
+</html>
